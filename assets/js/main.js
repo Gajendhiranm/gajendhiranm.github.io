940731408b3477792a2d51c46ba3353c1fc379ad
Gajendhiranm <48618592+Gajendhiranm@users.noreply.github.com>
2020-09-10 15:43:25 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/*Sweet alert */
+function alerting(){
+    Swal.fire({
+        title: 'Whatsapp ',
+        text: '80988 60669',
+        imageUrl: 'https://image.flaticon.com/icons/svg/220/220236.svg',
+        imageWidth: 100,
+        imageHeight: 100,
+     
+      })
+}
